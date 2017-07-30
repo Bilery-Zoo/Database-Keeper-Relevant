f13d52937daa71c4de6a7b602e0405b16ab5c13d
Bilery-Zoo <652645572@qq.com>
2017-07-30 17:38:04 +0800

    add table_crossdatabase_backup.sql

---
+++ table_crossdatabase_backup.sql
@@ -0,0 +1,41 @@
+delimiter $$$
+
+
+CREATE PROCEDURE tab_crossdb_bak(IN p_srcdb VARCHAR(25), IN p_baktb VARCHAR(25), IN p_tardb VARCHAR(25))
+	COMMENT 'Table `p_srcdb.p_srctb` cross database backup to `p_srcdb.p_tardb`'
+	/* create_author: Bilery Zoo(652645572@qq.com)
+	   create_time  : 2017-07-30 */
+	NOT DETERMINISTIC
+	CONTAINS SQL
+	BEGIN
+	
+	
+		DECLARE CONTINUE HANDLER FOR NOT FOUND, SQLEXCEPTION SET @_err := 1;
+		
+		SET @dro := CONCAT('DROP TABLE IF EXISTS `', p_tardb, '`.`', p_baktb, '`;');
+		SET @cre := CONCAT('CREATE TABLE `', p_tardb, '`.`', p_baktb, '` LIKE `', p_srcdb, '`.`', p_baktb, '`;');
+		SET @ins := CONCAT('INSERT INTO `', p_tardb, '`.`', p_baktb, '` SELECT * FROM `', p_srcdb, '`.`', p_baktb, '`;');
+		
+		START TRANSACTION;
+		
+			-- do drop
+			PREPARE DRO FROM @dro;
+			EXECUTE DRO;
+			
+			-- do create
+			PREPARE CRE FROM @cre;
+			EXECUTE CRE;
+			
+			-- do insert
+			PREPARE INS FROM @ins;
+			EXECUTE INS;
+			
+		IF @_err = 1 THEN ROLLBACK;
+		ELSE COMMIT;
+		END IF;
+	
+	
+	END $$$
+	
+
+delimiter ;
--- table_rowcount_function.sql
+++ table_rowcount_function.sql
@@ -6,7 +6,7 @@ CREATE FUNCTION tab_rows_cnt(p_db VARCHAR(25), p_tb VARCHAR(25))
 	COMMENT 'Table `p_db.p_tb` records Count(MyISAM) or Estimate(InnoDB)'
 	/* create_author: Bilery Zoo(652645572@qq.com)
 	   create_time  : 2017-07-30 */
-	DETERMINISTIC
+	NOT DETERMINISTIC
 	READS SQL DATA
 	BEGIN
 
